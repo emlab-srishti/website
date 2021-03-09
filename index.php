169f641a2c15decdad88c82d092f4d208d0a0e18
0xf17 <grv@mathscapes.xyz>
2021-03-09 09:06:04 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!-- <?php
+include 'config.php';
+?> -->
+
+<?php include 'partials/pre-load.php'; ?>
+
+<body>
+    <div class="container-fluid">
+
+        <!-- HEADER -->
+        <div class="row align-items-start">
+            <h1 class="sans"><a class="title" href="/srishti/experimental-maths-lab">Experimental Maths Lab at Srishti</a>
+                <?php include 'partials/dark-theme-toggle.php'; ?></h1>
+        </div>
+
+        <!-- CONTENT-TOP -->
+        <div class="row align-items start mt-3 mb-5">
+            <div class="col-lg-6">
+                <p class="serif big">Playground for exploring world around us through maths and computation</p>
+            </div>
+            <div class="col-lg-6"></div>
+            <div class="col-lg-2">
+            </div>
+        </div>
+
+        <div class="row align-items start mt-3">
+            <div class="col-lg-12">
+                <p class="sans big">Coming up next</p>
+                <p class="serif">Mathematics of Magnificent Flora</p>
+            </div>
+        </div>
+
+    </div>
+    <?php include 'partials/post-load.php'; ?>
+</body>
+
+</html>
