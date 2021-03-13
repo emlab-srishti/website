7dbfdc37b4be0b9062117cdbbd2f1c880d2842b6
0xf17 <grv@mathscapes.xyz>
2021-03-13 11:51:46 +0530

    magflora updated

---
+++ magflora.php
@@ -0,0 +1,56 @@
+<!-- <?php
+include 'config.php';
+?> -->
+
+<?php include 'partials/pre-load.php'; ?>
+
+<body>
+    <div class="container-fluid">
+
+        <!-- HEADER -->
+        <div class="row align-items-start">
+            <h1 class="sans"><a class="title" href="/srishti/experimental-maths-lab">Mathematics of Magnificent Flora</a>
+                <?php include 'partials/dark-theme-toggle.php'; ?></h1>
+        </div>
+
+        <div class="row align-items start mt-3">
+            <div class="col-lg-12"> <img src="mf/1.png"> </div>
+            <div class="col-lg-12"> <img src="mf/2.png"> </div>
+            <div class="col-lg-12"> <img src="mf/3.png"> </div>
+            <div class="col-lg-12"> <img src="mf/4.png"> </div>
+            <div class="col-lg-12"> <img src="mf/5.png"> </div>
+            <div class="col-lg-12"> <img src="mf/6.png"> </div>
+            <div class="col-lg-12"> <img src="mf/7.png"> </div>
+            <div class="col-lg-12"> <img src="mf/8.png"> </div>
+            <div class="col-lg-12"> <img src="mf/9.png"> </div>
+            <div class="col-lg-12"> <img src="mf/10.png"> </div>
+            <div class="col-lg-12"> <img src="mf/11.png"> </div>
+            <div class="col-lg-12"> <img src="mf/12.png"> </div>
+            <div class="col-lg-12"> <img src="mf/13.png"> </div>
+            <div class="col-lg-12"> <img src="mf/14.png"> </div>
+            <div class="col-lg-12"> <img src="mf/15.png"> </div>
+            <div class="col-lg-12"> <img src="mf/16.png"> </div>
+            <div class="col-lg-12"> <img src="mf/17.png"> </div>
+            <div class="col-lg-12"> <img src="mf/18.png"> </div>
+            <div class="col-lg-12"> <img src="mf/19.png"> </div>
+            <div class="col-lg-12"> <img src="mf/20.png"> </div>
+            <div class="col-lg-12"> <img src="mf/21.png"> </div>
+            <div class="col-lg-12"> <img src="mf/22.png"> </div>
+            <div class="col-lg-12"> <img src="mf/23.png"> </div>
+            <div class="col-lg-12"> <img src="mf/24.png"> </div>
+            <div class="col-lg-12"> <img src="mf/25.png"> </div>
+            <div class="col-lg-12"> <img src="mf/26.png"> </div>
+            <div class="col-lg-12"> <img src="mf/27.png"> </div>
+            <div class="col-lg-12"> <img src="mf/28.png"> </div>
+            <div class="col-lg-12"> <img src="mf/29.png"> </div>
+            <div class="col-lg-12"> <img src="mf/30.png"> </div>
+            <div class="col-lg-12"> <img src="mf/31.png"> </div>
+            <div class="col-lg-12"> <img src="mf/32.png"> </div>
+            <div class="col-lg-12"> <img src="mf/33.png"> </div>
+        </div>
+
+    </div>
+    <?php include 'partials/post-load.php'; ?>
+</body>
+
+</html>
