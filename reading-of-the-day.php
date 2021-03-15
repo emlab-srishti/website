9c77f37e94eb900164e29b229a36f57f680579f8
0xf17 <grv@mathscapes.xyz>
2021-03-15 15:36:20 +0530

    rod update

---
+++ reading-of-the-day.php
@@ -0,0 +1,36 @@
+<!-- <?php
+include 'config.php';
+?> -->
+
+<?php include 'partials/pre-load.php'; ?>
+
+<body>
+    <div class="container-fluid">
+
+        <!-- HEADER -->
+        <div class="row align-items-start">
+            <h1 class="sans"><a class="title" href="/srishti/experimental-maths-lab">Experimental Maths Lab at Srishti</a>
+                <?php include 'partials/dark-theme-toggle.php'; ?></h1>
+        </div>
+
+        <!-- CONTENT-TOP -->
+        <div class="row align-items start mt-3 mb-5">
+            <div class="col-lg-6">
+                <p class="serif big">Reading of the Day</p>
+            </div>
+            <div class="col-lg-6"></div>
+            <div class="col-lg-2">
+            </div>
+        </div>
+
+        <div class="row align-items start mt-3">
+            <div class="col-lg-12">
+                <p class="serif">15th March 2020 - First four pages from A Mathematician’s Lament by Paul Lockhart</p>
+            </div>
+        </div>
+
+    </div>
+    <?php include 'partials/post-load.php'; ?>
+</body>
+
+</html>
